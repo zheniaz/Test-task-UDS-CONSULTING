8bc6600557b5a71462676431e4d6a1d5879d00c4
Zhenia Zgurovets <zhenia.zgurovets@gmail.com>
2016-07-16 17:26:51 +0300

    release

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime returnDate = startDate;
+            Console.WriteLine(returnDate);
+            DateTime endDateCalc = startDate.AddDays(dayCount);
+            DateTime tempDate;
+
+            List<DateTime> tempWeekDays = new List<DateTime>();
+            if (weekEnds != null)
+            {
+                // создаем список выходных, которые попадают в рассматриваемое время 
+                foreach (var item in weekEnds)
+                {
+                    int weekDays = 0;
+                    // вычисляем, если несколько подряд выходных или тольно один
+                    if (item.StartDate != item.EndDate)
+                    {
+                        tempDate = item.StartDate;
+                        do
+                        {
+                            weekDays++;
+                            tempDate = tempDate.AddDays(1);
+                        }
+                        while (tempDate != item.EndDate.AddDays(1));
+                    }
+                    else weekDays = 1;
+
+                    tempDate = item.StartDate;
+                    // формируем список выходных, которые попадают в рассматриваемое время 
+                    for (int i = 0; i < weekDays; i++)
+                    {
+                        if (startDate <= tempDate && tempDate <= endDateCalc)
+                        {
+                            tempWeekDays.Add(tempDate);
+                        }
+
+                        tempDate = tempDate.AddDays(1);
+                    }
+                }
+
+                // Проходим по каждому дню, начиная с startDate и до endDateCalc отсеивая все выходные
+                tempDate = startDate;
+                bool dateIn = false;
+                while (tempDate != endDateCalc.AddDays(1))
+                {
+                    if (tempWeekDays.Contains(tempDate))
+                    {
+                        if (tempDate == startDate)
+                            returnDate = returnDate.AddDays(-1);
+                        tempDate = tempDate.AddDays(1);
+                    }
+                    else
+                    {
+                        if (tempDate != startDate)
+                        {
+                            returnDate = returnDate.AddDays(1);
+                            tempDate = tempDate.AddDays(1);
+                            dateIn = true;
+                        }
+                        else
+                        {
+                            tempDate = tempDate.AddDays(1);
+                            dateIn = true;
+                        }
+                    }
+                }
+                if (dateIn)
+                    return returnDate;
+                else
+                    return returnDate.AddDays(1);
+            }
+            else
+            {
+                returnDate = returnDate.AddDays(dayCount);
+            }
+
+            return returnDate;
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculatorTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+
+            Assert.AreEqual(startDate.AddDays(count), result);
+        }
+
+        [TestMethod]
+        public void TestWeekEnd()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+            WeekEnd[] week =
+            {
+                new WeekEnd(startDate,startDate)
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, week);
+
+            Assert.AreEqual(startDate.AddDays(count - 1), result);
+        }
+
+        // в случае если все дни выходные, не очень ясно какой должен быть возвзврат, 
+        // и потому я выбрал качестве возврата дата начала + 0
+        [TestMethod]
+        public void TestWeekAll()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            DateTime endDate = new DateTime(2014, 12, 11);
+            int count = 10;
+            WeekEnd[] week =
+            {
+                new WeekEnd(startDate,endDate)
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, week);
+
+            Assert.AreEqual(startDate, result);
+        }
+
+        [TestMethod]
+        public void TestWeekPart()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+            WeekEnd[] week =
+            {
+                new WeekEnd(new DateTime(2014, 12, 5), new DateTime(2014, 12, 15))
+            };
+            
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, week);
+
+            Assert.AreEqual(startDate.AddDays(3), result);
+        }
+
+        [TestMethod]
+        public void TestWeekDays()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 30;
+            WeekEnd[] week =
+            {
+                new WeekEnd(new DateTime(2014, 12, 6), new DateTime(2014, 12, 7)),
+                new WeekEnd(new DateTime(2014, 12, 13), new DateTime(2014, 12, 14)),
+                new WeekEnd(new DateTime(2014, 12, 20), new DateTime(2014, 12, 21)),
+                new WeekEnd(new DateTime(2014, 12, 27), new DateTime(2014, 12, 28))
+            };
+
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, week);
+
+            Assert.AreEqual(startDate.AddDays(22), result);
+        }
+
+    }
+}
